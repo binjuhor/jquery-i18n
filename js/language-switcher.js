8e2131df05a9971ab0c4fe33d41753fdc0e01878
Binjuhor <binjuhor@gmail.com>
2019-06-25 13:37:30 +0700

    v0.1 - Language switcher on URL

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -1,22 +1,22 @@
-function updateText() {
-	'use strict'
-	var i18n = $.i18n(), language
+function updateText () {
+  'use strict'
+  let i18n = $.i18n(), language
 
-	language = $( '.language option:selected' ).val()
+  language = url('?lang')
 
-	i18n.locale = language;
-	i18n.load( 'i18n/' + i18n.locale + '.json', i18n.locale ).done(
-		function () {
-			$('html *').i18n()
-		} );
+  if (!language) {
+    History.pushState(null, null, "?lang=en" )
+    language = 'en'
+  }
+
+  i18n.locale = language
+  i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(
+    function () {
+      $('html *').i18n()
+    })
 }
-// Enable debug
-$.i18n.debug = true
 
-$( document ).ready( function ( $ ) {
-	'use strict'
-	updateText()
-
-	//Action for change the text
-	$( '.language' ).on( 'change keyup', updateText )
-} );
+( function ($) {
+  'use strict'
+  updateText()
+} )(jQuery)
