37804944edc1381248562573b5c4a22333d370ec
Binjuhor <binjuhor@gmail.com>
2019-06-25 13:46:30 +0700

    0.1 - Update Available Language and default language

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -1,12 +1,18 @@
 function updateLanguage () {
   'use strict'
+  let availableLanguage = [ 'vi', 'en', 'cn' ]
+  let defaultLanguage = 'en'
   let i18n = $.i18n(), language
 
   language = url('?lang')
 
   if (!language) {
-    History.pushState(null, null, "?lang=en" )
-    language = 'en'
+    History.pushState(null, null, '?lang=' + defaultLanguage)
+    language = defaultLanguage
+  }
+
+  if (language && availableLanguage.indexOf(language) === -1) {
+    language = defaultLanguage
   }
 
   i18n.locale = language
